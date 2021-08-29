efaba880507122976bdb5cf3da950154394b4edb
maria <marianievas11@gmail.com>
2021-08-29 12:38:27 +0200

    sign up and log in in process

--- app/src/main/java/com/myerasmus/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/myerasmus/ui/signup/SignUpActivity.kt
@@ -2,8 +2,11 @@ package com.myerasmus.ui.signup
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -12,13 +15,17 @@ import com.google.firebase.auth.FirebaseAuth
 import com.myerasmus.MainActivity
 import com.myerasmus.ProviderType
 import com.myerasmus.R
+import com.myerasmus.ui.login.LoginActivity
+import org.w3c.dom.Text
 
-class SignUpActivity: AppCompatActivity() {
+class SignUpActivity: AppCompatActivity(), View.OnClickListener{
 
     private lateinit var signUpBtn : Button
     private lateinit var editTextUsername : EditText
     private lateinit var editTextEmail : EditText
     private lateinit var editTextPassword : EditText
+    private lateinit var goLogin : TextView
+    private lateinit var loading : ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_MyErasmus)
@@ -30,22 +37,44 @@ class SignUpActivity: AppCompatActivity() {
         editTextUsername = findViewById(R.id.editTextUsername)
         editTextEmail = findViewById(R.id.editTextEmail2)
         editTextPassword = findViewById(R.id.editTextPassword2)
+        goLogin = findViewById(R.id.textViewHere)
+        loading = findViewById(R.id.loading)
 
         val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
         bundle.putString("message", "Firebase integration completed")
         analytics.logEvent("initScreen", bundle)
 
-        setup()
+        /*signUpBtn.setOnClickListener {
+            Log.d(String.toString(), "AAAAAAAAAAAA")
+            loading.visibility = View.VISIBLE
+            if (editTextEmail.text.isNotEmpty() && editTextUsername.text.isNotEmpty() && editTextPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString()).addOnCompleteListener {
+                    //if (it.isSuccessful) showHome(it.result?.user?.email ?: "", editTextUsername.text.toString(), ProviderType.BASIC)
+                    if (it.isSuccessful) {
+                        onBackPressed()
+                        // val homeIntent = Intent(this, LoginActivity::class.java)
+                        // startActivity(homeIntent)
+                    } else showAlert()
+                }
+            }
+        }*/
+        signUpBtn.setOnClickListener(this)
+        goLogin.setOnClickListener {
+            onBackPressed()
+        }
     }
 
-    private fun setup(){
-
-        signUpBtn.setOnClickListener{
-            if (editTextEmail.text.isNotEmpty() && editTextUsername.text.isNotEmpty() && editTextPassword.text.isNotEmpty()){
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString()).addOnCompleteListener {
-                    if (it.isSuccessful) showHome(it.result?.user?.email ?: "", editTextUsername.text.toString(), ProviderType.BASIC)
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.ButtonSignUp->{
+                Log.d(String.toString(), "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                loading.visibility = View.VISIBLE
+                if (editTextEmail.text.isNotEmpty() && editTextUsername.text.isNotEmpty() && editTextPassword.text.isNotEmpty()) {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) onBackPressed()
                     else showAlert()
+                    }
                 }
             }
         }
@@ -61,7 +90,7 @@ class SignUpActivity: AppCompatActivity() {
     }
 
     private fun showHome(email: String, username: String, provider: ProviderType){
-        val homeIntent: Intent = Intent(this, MainActivity::class.java).apply {
+        val homeIntent: Intent = Intent(this, LoginActivity::class.java).apply {
             putExtra("email", email)
             putExtra("username", username)
             putExtra("provider", provider.name)
