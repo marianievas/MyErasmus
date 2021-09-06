ae857835b6759eac678d7c9761116499dea2dcc4
maria <marianievas11@gmail.com>
2021-09-06 12:30:53 +0200

    loading photo from Firebase storage

--- app/src/main/java/com/myerasmus/common/Constants.kt
+++ app/src/main/java/com/myerasmus/common/Constants.kt
@@ -2,6 +2,8 @@ package com.myerasmus.common
 
 data class Constants(
 
+    val URL_STORAGE: String = "gs://myerasmus-7dc3a.appspot.com",
+
     // Preferences
     val PREF_EMAIL: String = "PREF_EMAIL",
     val PREF_USERNAME: String = "PREF_NAME",
@@ -9,6 +11,8 @@ data class Constants(
     val PREF_PROVIDER_PASSWORD: String = "PASSWORD",
     val PREF_PROVIDER_GOOGLE: String = "GOOGLE",
     val PREF_UID: String = "PREF_UID",
+    val PICK_PHOTO_FOR_AVATAR: Int = 1,
+    val SELECT_PHOTO_GALLERY: Int = 1,
     //val PREF_PHOTO: String = "URL_PHOTO",
     //val PICK_PHOTO_FOR_AVATAR: Int = 1,
    // val SELECT_PHOTO_GALLERY: Int = 1,
--- app/src/main/java/com/myerasmus/ui/createEntryDiary/CreateEntryDiary.kt
+++ app/src/main/java/com/myerasmus/ui/createEntryDiary/CreateEntryDiary.kt
@@ -1,13 +1,25 @@
 package com.myerasmus.ui.createEntryDiary
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.myerasmus.MainActivity
 import com.myerasmus.R
+import com.myerasmus.common.Constants
 import kotlinx.android.synthetic.main.new_entry_diary.*
 import kotlinx.android.synthetic.main.new_entry_diary.view.*
 import java.util.*
@@ -20,6 +32,7 @@ class CreateEntryDiary: AppCompatActivity()  {
     private lateinit var btnAdd: Button
     private lateinit var datePicker: Button
     private lateinit var date: TextView
+    private lateinit var mainPhoto: ImageView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +51,7 @@ class CreateEntryDiary: AppCompatActivity()  {
         datePicker = findViewById(R.id.btn_pickDate)
         date = findViewById(R.id.tvDate)
         btnAdd = findViewById(R.id.btn_create)
+        mainPhoto = findViewById(R.id.imageViewPic)
 
 
         btnAdd.setOnClickListener {
@@ -64,7 +78,76 @@ class CreateEntryDiary: AppCompatActivity()  {
             }, year, month, day)
             dpd.show()
         }
+
+        Glide.with(this)
+                .load(Constants().URL_STORAGE + "upload/userimageget/")
+                .placeholder(R.drawable.friends).centerCrop().circleCrop()
+                .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true)
+                .into(imageViewPic)
+
+        mainPhoto.setOnClickListener{
+            Dexter.withContext(this)
+                    .withPermissions(
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                    )
+                    .withListener(object : MultiplePermissionsListener {
+                        override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                            // check if all permissions are granted
+                            if (report.areAllPermissionsGranted()) {
+                                val pickPhoto = Intent(
+                                        Intent.ACTION_PICK,
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                                )
+                                startActivityForResult(pickPhoto, Constants().SELECT_PHOTO_GALLERY)
+                            }
+                            // check for permanent denial of any permission
+                            if (report.isAnyPermissionPermanentlyDenied) {
+                                // permission is denied permenantly, navigate user to app settings
+                            }
+                        }
+
+                        override fun onPermissionRationaleShouldBeShown(
+                                permissions: List<PermissionRequest>,
+                                token: PermissionToken
+                        ) {
+                            token.continuePermissionRequest()
+                        }
+                    })
+                    .onSameThread()
+                    .check()
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constants().PICK_PHOTO_FOR_AVATAR && resultCode == AppCompatActivity.RESULT_OK) {
+            val photoPath = Constants().URL_STORAGE + "/pics"
+            Glide.with(this).load(photoPath).centerCrop().circleCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true)
+                    .into(mainPhoto)
+           /* if (data != null) {
+                val imageSelected = data.data
+                val filepathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                val cursor: Cursor? = requireContext().contentResolver.query(
+                        imageSelected!!,
+                        filepathColumn,
+                        null,
+                        null,
+                        null
+                )
+                if (cursor != null) {
+                    cursor.moveToFirst()
+                    val imageIndex: Int = cursor.getColumnIndex(filepathColumn[0])
+                    val photoPath: String = cursor.getString(imageIndex)
+                            Glide.with(this).load(photoPath).centerCrop().circleCrop()
+                                    .diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true)
+                                    .into(mainPhoto)
+                            cursor.close()
+                }
+            }*/
+        }
+    }
+
 
 
     private fun setCustomAdapter() {
